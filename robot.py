222432d98da8fa23fa8810069bf786ff01e5bfd9
Ehud9911 <132237776+Ehud9911@users.noreply.github.com>
2026-06-15 05:53:38 +0200

    Disable OneDNN optimizations for TensorFlow

--- robot.py
+++ robot.py
@@ -1,6 +1,8 @@
 import os
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
+
 import numpy as np
 import pandas as pd
 import yfinance as yf
